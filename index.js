a0e47b550dc1f84fa2e399993e629ed6426982bc
Kyrian <dr.kyrian.onwe@gmail.com>
2022-02-18 12:54:42 -0600

    commit tree f9a4724cac3bd1470a5962625de73391bd2d173d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function createEmployeeRecord(array){
+    
+        const obj = {
+            "firstName": array[0],
+            "familyName": array[1],
+            "title": array[2],
+            "payPerHour": array[3],
+            "timeInEvents": [],
+            "timeOutEvents": []
+
+        }
+        return obj
+
+    };
+
+function createEmployeeRecords(){};
+
+
+    /*const timeInEvents = [];
+    const timeOutEvents = [];
+    let theForm = document.createElement('div')
+    theForm.className = 'form';
+    theForm.innerHTML = `
+        <h3></h3>`*/
+
+
+
